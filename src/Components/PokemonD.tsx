55f840e90ac419968a482799cb960af45cc209e5
Gabrierh0h0 <gabriudam@gmail.com>
2025-04-26 18:52:08 -0500

    Aver si la imagen deja de poderse buscar

--- src/Components/PokemonD.tsx
+++ src/Components/PokemonD.tsx
@@ -8,11 +8,12 @@ interface PokemonDisplayProps {
 const PokemonDisplay: React.FC<PokemonDisplayProps> = ({ sprite, gameOver }) => {
   return (
     <div className="mb-6 relative w-40 h-40 mx-auto">
-      <div className="absolute inset-0 rounded-full bg-gradient-to-b from-gray-900 to-black shadow-inner opacity-80"></div>
+      <div className="absolute inset-0"></div>
       <div
         className={`w-40 h-40 mx-auto transition-all duration-500 ${
           gameOver ? 'filter brightness-100 scale-110' : 'filter brightness-0 scale-100'
         }`}
+        draggable = "false"
         style={{
           backgroundImage: `url(${sprite})`,
           backgroundSize: 'contain',
@@ -20,6 +21,7 @@ const PokemonDisplay: React.FC<PokemonDisplayProps> = ({ sprite, gameOver }) => 
           backgroundRepeat: 'no-repeat',
           userSelect: 'none', // Evita selección
         }}
+        
       />
     </div>
   );
